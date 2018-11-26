60f3fa9a87ee884c573d511d9052855a43634376
suparuttanatree <45094634+suparuttanatree@users.noreply.github.com>
2018-11-26 23:37:19 +0700

    LAB06_593021027-8_SEC2

---
+++ app/src/main/java/com/example/hp/lab6/Student.java
@@ -0,0 +1,27 @@
+package com.example.hp.lab6;
+
+
+public class Student {
+    public static final String TABLE_NAME = "students";
+    public static final String COLUMN_ID = "std_id";
+    public static final String COLUMN_NAME = "std_name";
+    private String id;
+    private String name;
+    public static final String CREATE_TABLE =
+            "CREATE TABLE " + TABLE_NAME + "("
+                    + COLUMN_ID + " TEXT,"
+                    + COLUMN_NAME + " TEXT"
+                    + ")";
+    public Student(String id,String name){
+        this.id = id;
+        this.name = name;
+    }
+    public Student(){}
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id){this.id = id;}
+    public String getName() { return name; }
+    public void setName(String name){ this.name = name;}
+}
